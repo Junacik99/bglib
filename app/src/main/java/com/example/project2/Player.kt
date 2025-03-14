3cf9f5a8c0e7999fce71eb6faed52bd19e4e011f
Junacik99 <junacik99@gmail.com>
2025-03-14 22:45:51 +0100

    player

---
+++ app/src/main/java/com/example/project2/Player.kt
@@ -0,0 +1,37 @@
+package com.example.project2
+
+class Player (id: Int) {
+    // Name
+    var name: String = ""
+    // Score
+    var score: Int = 0
+    // Move count
+    var moveCount: Int = 0
+    // Cards
+    val cards: MutableList<Card> = mutableListOf()
+
+    init {
+
+    }
+
+    fun addCard(card: Card) {
+        this.cards.add(card)
+    }
+    fun removeCard(card: Card) {
+        this.cards.remove(card)
+    }
+
+    fun updateMove(){
+        this.moveCount++
+        // TODO: Add logic after each move
+    }
+
+    fun updateScore(score: Int) {
+        this.score += score
+    }
+
+    fun resetScore() {
+        this.score = 0
+    }
+
+}
