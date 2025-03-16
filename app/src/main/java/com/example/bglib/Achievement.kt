a84b2f3666780bf8384934b1e962bd5b382a3288
Junacik99 <junacik99@gmail.com>
2025-03-16 22:17:36 +0100

    Achievement class

---
+++ app/src/main/java/com/example/bglib/Achievement.kt
@@ -0,0 +1,66 @@
+package com.example.bglib
+
+// Achievement class designed as in the article from Juho Hamari and Veikko Eranti
+// Framework for Designing and Evaluating Game Achievements (2011)
+class Achievement (
+    val name: String,                                   // Name of the achievement
+    val description: String,                            // Description of the achievement
+    val imageLocked: Int,                               // Image to be displayed when the achievement is still locked
+    val imageUnlocked: Int,                             // Image to be displayed when the achievement is unlocked
+    val trigger: () -> Boolean,                         // Action to check if the achievement is triggered
+    val requirements: List<() -> Boolean>,              // List of requirements of availability or because the achievement requires it
+    val conditions: List<() -> Boolean>,                // List of conditions of the achievement (eg. is player using weapon x?)
+    val multiplier: Int,                                // How many times should the trigger be triggered in order to unlock the achievement
+    val reward: () -> Unit                              // An award action to be performed when the achievement is unlocked
+    ) {
+
+    var unlocked = false
+    var image = imageLocked
+    var progress = 0
+
+    fun checkRequirements(): Boolean {
+        if (requirements.isEmpty()) return true
+        for (requirement in requirements) {
+            if (!requirement()) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun checkConditions(): Boolean {
+        if (conditions.isEmpty()) return true
+        for (condition in conditions) {
+            if (!condition()) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun checkTrigger(): Boolean {
+        return checkRequirements() && checkConditions() && trigger()
+    }
+
+    fun unlock() {
+        unlocked = true
+        image = imageUnlocked
+        reward()
+    }
+
+    fun lock() {
+        unlocked = false
+        image = imageLocked
+    }
+
+    fun check(){
+        if (unlocked) return
+        if (checkTrigger()) {
+            progress++
+        }
+        if (progress >= multiplier) {
+            unlock()
+        }
+    }
+
+}
